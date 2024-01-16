bf465f65c1dc79f76d79f58780dfc807ea3bb5c1
Adam Monsen <haircut@gmail.com>
2024-01-16 14:46:21 -0800

    add extension: image at each chapter
    followed: https://docs.asciidoctor.org/pdf-converter/latest/extend/use-cases/#chapter-image
    
    printer PNG generated with https://emoji.aranja.com

--- book/.internal-build.sh
+++ book/.internal-build.sh
@@ -24,6 +24,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-bundle exec asciidoctor-pdf --attribute build_date_time="$BUILD_DATE_TIME" --attribute build_git_commit="$BUILD_GIT_COMMIT" --attribute build_os_release="$BUILD_OS_RELEASE" --warnings --trace print-this.adoc
+bundle exec asciidoctor-pdf --attribute build_date_time="$BUILD_DATE_TIME" --attribute build_git_commit="$BUILD_GIT_COMMIT" --attribute build_os_release="$BUILD_OS_RELEASE" --warnings --trace --require ./extensions.rb print-this.adoc
 
 cp print-this.pdf /outputs
