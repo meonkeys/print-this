4149137ef028ff69e280d1fd4332d24b675961b1
Adam Monsen <haircut@gmail.com>
2024-01-17 11:39:01 -0800

    improve docs for ink_running_content

--- book/extensions.rb
+++ book/extensions.rb
@@ -11,14 +11,17 @@ class ExtendedPDFConverter < (Asciidoctor::Converter.for 'pdf')
     super
   end
 
-  # Loop over all pages.
-  # Add something to empty pages, e.g. an invisible glyph (maybe just an ASCII 0x20 space character?).
+  # Add one invisible character to pages only containing running content.
+  # This allows using `media: prepress` with some printers that don't like these sorta empty pages (e.g. Lulu).
   #
   # per Dan Allen:
   # > The likely culprit is the running content on otherwise empty pages.
   # > It's possible their analyzer thinks that is invalid for some reason (if it only sees a PDF stamp, which is what the running content is).
   #
-  # discussion at https://asciidoctor.zulipchat.com/#narrow/stream/288690-users.2Fasciidoctor-pdf/topic/syntax.20error.20with.20prepress.20PDF.20on.20Lulu
+  # See also:
+  # * discussion https://asciidoctor.zulipchat.com/#narrow/stream/288690-users.2Fasciidoctor-pdf/topic/syntax.20error.20with.20prepress.20PDF.20on.20Lulu
+  # * https://github.com/asciidoctor/asciidoctor-pdf/issues/2476
+  # * https://github.com/asciidoctor/asciidoctor-pdf/issues/2477
   def ink_running_content periphery, doc, skip = [1, 1], body_start_page_number = 1
     super
 
