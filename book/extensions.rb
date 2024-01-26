154ee3a6360957c3112aeba5ce2b86e7a6cb247f
Adam Monsen <haircut@gmail.com>
2024-01-26 07:19:06 -0800

    BROKEN - demo issue with ToC + Colophon

--- book/extensions.rb
+++ book/extensions.rb
@@ -9,6 +9,25 @@ class ExtendedPDFConverter < (Asciidoctor::Converter.for 'pdf')
     convert_image image_block, relative_to_imagesdir: true, pinned: true
     super
   end
+
+  # Render Colophon before TOC.
+  # see https://docs.asciidoctor.org/pdf-converter/latest/extend/use-cases/#colophon-before-toc
+  def ink_toc doc, num_levels, toc_page_number, start_cursor, num_front_matter_pages = 0
+    colophon = (doc.instance_variable_get :@colophon) || (doc.sections.find {|sect| sect.sectname == 'colophon' })
+    return super unless colophon
+    go_to_page toc_page_number unless (page_number == toc_page_number) || scratch?
+    if scratch?
+      (doc.instance_variable_set :@colophon, colophon).parent.blocks.delete colophon
+    else
+      # if doctype=book and media=prepress, use blank page before table of contents
+      go_to_page page_number.pred if @ppbook
+      convert_section colophon
+      go_to_page page_number.next
+    end
+    offset = @ppbook ? 0 : 1
+    toc_page_numbers = super doc, num_levels, (toc_page_number + offset), start_cursor, num_front_matter_pages
+    scratch? ? ((toc_page_numbers.begin - offset)..toc_page_numbers.end) : toc_page_numbers
+  end
 end
 
 # Allow long words to wrap at slashes.
