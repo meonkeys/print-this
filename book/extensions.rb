d59c7c037a595ecbed31893dae1a46ca6eeff6b4
Adam Monsen <haircut@gmail.com>
2024-01-27 18:03:17 -0800

    Revert "broken on purpose: demo crowded index"
    This reverts commit 67c26ffc6a4e5ba39313fa3e894e0611f9ea06ef.
    
    The image margins prepress issue is fixed and soon to be released! See https://github.com/asciidoctor/asciidoctor-pdf/issues/2480
    
    Thank you Dan Allen!

--- book/extensions.rb
+++ book/extensions.rb
@@ -3,14 +3,12 @@ class ExtendedPDFConverter < (Asciidoctor::Converter.for 'pdf')
 
   # Add an image at the start of each chapter.
   # see https://docs.asciidoctor.org/pdf-converter/latest/extend/use-cases/#chapter-image
-  #def ink_chapter_title sect, title, opts
-  #  if sect.sectname != 'index'
-  #    image_attrs = { 'target' => 'printer.png', 'pdfwidth' => '1in' }
-  #    image_block = ::Asciidoctor::Block.new sect.document, :image, content_model: :empty, attributes: image_attrs
-  #    convert_image image_block, relative_to_imagesdir: true, pinned: true
-  #  end
-  #  super
-  #end
+  def ink_chapter_title sect, title, opts
+    image_attrs = { 'target' => 'printer.png', 'pdfwidth' => '1in' }
+    image_block = ::Asciidoctor::Block.new sect.document, :image, content_model: :empty, attributes: image_attrs
+    convert_image image_block, relative_to_imagesdir: true, pinned: true
+    super
+  end
 end
 
 # Allow long words to wrap at slashes.
